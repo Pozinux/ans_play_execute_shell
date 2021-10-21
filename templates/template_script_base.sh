67126646803715af89a56170c9e76bc64fcaff58
Timothé Poznanski <tim.poznanski@gmail.com>
2021-10-21 10:22:09 +0200

    Add files via upload

--- templates/template_script_base.sh
+++ templates/template_script_base.sh
@@ -1,4 +1,4 @@
 lscpu | grep -e '^CPU(s)' | awk '{print "CPU : " $2}'
 free -h | grep Mem | awk '{print "Mémoire : " $2}'
-# free -h | grep Swap | awk '{print "Swap : " $2}'
+free -h | grep Swap | awk '{print "Swap : " $2}'
 echo
